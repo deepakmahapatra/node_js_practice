e017083bdc5a156229c21c14b6dd7f800736a956
deepakmahapatra <deepakmahapatra54@gmail.com>
2020-10-21 22:27:53 -0500

    socket io two way communication added

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,21 @@
-io()
+const socket = io()
+
+// socket.on('countupdated', (count) => {
+//     console.log("The count has been updated", count)
+// })
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
+
+
+document.querySelector('#message-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    const message = event.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -8,9 +8,32 @@ const port = process.env.PORT || 3000
 const server = http.createServer(app)
 const io = socketio(server)
 
-io.on('connect', ()=> {
+// let count = 0
+
+io.on('connect', (socket)=> {
     console.log("socket io connection received")
+
+    // socket.emit('countupdated', count)
+    socket.emit('message', 'Welcome')
+
+    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countupdated', count)
+    //     io.emit('countupdated', count)
+    // })
+
+    socket.on('disconnect', () => {
+        io.emit("message","A user has left")
+    })
 })
+
+
 server.listen(port, () => {
     console.log("Server is up ", port)
 })
